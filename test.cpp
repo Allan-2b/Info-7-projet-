e2e2053b50623e3553f144053080da8a948dc5a4
jerome lamotte <cj.pitiallan@orange.fr>
2026-03-23 09:15:48 +0100

    passage par pointeur

--- test.cpp
+++ test.cpp
@@ -19,9 +19,9 @@ struct Plateau{
     Case Tab[64];
 };
 
-void empty(Plateau& P) { 
+void empty(Plateau* P) { 
     for(int i = 0; i < 64; i++){
-        P.Tab[i].contenu = Vide;
+        P->Tab[i].contenu = Vide;
     }
 }
 
@@ -31,56 +31,56 @@ Case get_square(Plateau P, Case c){
 }
 
 
-void set_square(Plateau& P, Case c, pieces_type pi){
-    P.Tab[c.coordonee].contenu = pi;
+void set_square(Plateau* P, Case c, pieces_type pi){
+    P->Tab[c.coordonee].contenu = pi;
 }
 
-void start(Plateau& P){
+void start(Plateau* P){
     empty(P);
     for(int i = 0; i < 64; i++){
         if (i > 7 and i < 16){
-            P.Tab[i].contenu = PB;
+            P->Tab[i].contenu = PB;
         }
         else if (i > 47 and i < 56){
-            P.Tab[i].contenu = PW;
+            P->Tab[i].contenu = PW;
         }
         else if (i == 0 or i == 7){
-            P.Tab[i].contenu = RB;
+            P->Tab[i].contenu = RB;
         }
         else if (i == 56 or i == 63){
-            P.Tab[i].contenu = RW;
+            P->Tab[i].contenu = RW;
         }
         else if (i == 1 or i == 6){
-            P.Tab[i].contenu = NB;
+            P->Tab[i].contenu = NB;
         }
         else if (i == 57 or i == 62){
-            P.Tab[i].contenu = NW;
+            P->Tab[i].contenu = NW;
         }
         else if (i == 2 or i == 5){
-            P.Tab[i].contenu = BB;
+            P->Tab[i].contenu = BB;
         }
         else if (i == 58 or i == 61){
-            P.Tab[i].contenu = BW;
+            P->Tab[i].contenu = BW;
         }
         else if (i == 3){
-            P.Tab[i].contenu = QB;
+            P->Tab[i].contenu = QB;
         }
         else if (i == 59){
-            P.Tab[i].contenu = QW;
+            P->Tab[i].contenu = QW;
         }
         else if (i == 4){
-            P.Tab[i].contenu = KB;
+            P->Tab[i].contenu = KB;
         }
         else if (i == 60){
-            P.Tab[i].contenu = KW;
+            P->Tab[i].contenu = KW;
         }
 
 }
 }
 
-void move_piece(Plateau& P, Case c1, Case c2){
-    pieces_type pi = get_square(P,c1).contenu;
-    pieces_type pi2 = get_square(P,c2).contenu;
+void move_piece(Plateau* P, Case c1, Case c2){
+    pieces_type pi = get_square(*P,c1).contenu;
+    pieces_type pi2 = get_square(*P,c2).contenu;
     set_square(P,c2,pi);
     set_square(P,c1,pi2);
 }
@@ -88,22 +88,11 @@ void move_piece(Plateau& P, Case c1, Case c2){
 
 int main(){
     Plateau P;
-    start(P);
+    start(&P);
     for (int i=0; i<64; i++){
         cout << P.Tab[i].contenu << "\t";
         if((i+1) % 8 == 0) cout << endl; 
     }
-    cout << "test move" << endl;
-    Case c1 = {0, RB};
-    Case c2 = {16, Vide};
-    move_piece(P,c1,c2);
-    for (int i=0; i<64; i++){
-        cout << P.Tab[i].contenu << "\t";
-        if((i+1) % 8 == 0) cout << endl; 
-    }
-
-    
-    
 
     return 0;
 }
