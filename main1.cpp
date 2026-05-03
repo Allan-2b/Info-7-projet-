43bb58c20cca9927045a40e4b2b49ee137b420f4
Allan-2b <cj.pitiallan@orange.fr>
2026-05-03 12:52:24 +0200

    terminal linux

--- main1.cpp
+++ main1.cpp
@@ -2,9 +2,9 @@
 
 
 
-#ifdef _WIN32
-#include <windows.h>
-#endif
+//#ifdef _WIN32
+//#include <windows.h>
+//#endif
 
 
 #include "board.hpp"
@@ -19,9 +19,9 @@ using namespace std;
 int main(){
     
     /* Initialisation de la console pour windows */
-    #ifdef _WIN32
-    SetConsoleOutputCP(65001); 
-    #endif
+    //#ifdef _WIN32
+    //SetConsoleOutputCP(65001); 
+    //#endif
     
     game G;
     empty_mask(&G.M);
